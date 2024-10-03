e38ee43e03ece4f0288287fbdebcc1c9261e58a5
Abhishek14PS <abhishekprataps722@gmail.com>
2024-10-04 00:01:33 +0530

    13

---
+++ 13 Palindrome.py
@@ -0,0 +1,12 @@
+n = int(input("enter a number: "))
+original = n
+reverse = 0
+while n > 0:
+    digit = n % 10
+    reverse = reverse * 10 + digit
+    n //= 10
+if original == reverse:
+    print("palindrome")
+else:
+    print("not")
+    
